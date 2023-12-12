7e55417d3eadd97b412cbfa5822d438ac9360c3a
Dāniels Jansiņš <dj2782@gymn.local>
2023-12-12 10:26:31 +0200

    Otrais apildinājums

--- CO2_izmesi/Form1.Designer.cs
+++ CO2_izmesi/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
@@ -53,11 +55,29 @@
             this.label1.Text = "CO2 Izmeši";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(265, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(413, 109);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -72,6 +92,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- CO2_izmesi/Form1.cs
+++ CO2_izmesi/Form1.cs
@@ -7,9 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite; 
+
 
 namespace CO2_izmesi
 {
+
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,7 +23,7 @@ namespace CO2_izmesi
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -27,9 +31,28 @@ namespace CO2_izmesi
 
         }
 
+
+
+
         private void button1_Click(object sender, EventArgs e)
-        {
+        {   
+            /*static SQLiteConnection CreatConnection()
+            {
+                SQLiteConnection sqlite_conn;
+                sqlite_conn = new SQLiteConnection("Data Source=CO2_izmmesi_DB.db; Version = 3; New = True; Compress = True;");
+                try 
+                {
+                    sqlite_conn.Open();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                return sqlite_conn;*/
+            }
+
 
         }
     }
-}
+
+
